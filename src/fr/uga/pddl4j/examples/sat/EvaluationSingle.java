abcb95e22d2b699fc02062c0754430acb75112e2
Coemgen Genevey <coemgen.genevey@gmail.com>
2024-12-05 11:20:24 +0100

    premier commit

---
+++ src/fr/uga/pddl4j/examples/sat/EvaluationSingle.java
@@ -0,0 +1,29 @@
+package fr.uga.pddl4j.examples.sat;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EvaluationSingle {
+    public static void main(String[] args) {
+        /* 
+        String file = "p20.pddl";
+        String dossier="gripper";
+
+        
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dossier+"Time.txt", true))) {
+            writer.write("\n" + file+";");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(dossier+"Length.txt", true))) {
+            writer.write("\n" + file + ";");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MyPlanner.main(new String[] { ".\\"+dossier+"\\domain.pddl", ".\\"+dossier+"\\" + file, dossier+"Time.txt",
+                dossier+"Length.txt" });
+        */
+    }
+}
---
+++ src/fr/uga/pddl4j/examples/sat/ScriptEvaluation.java
@@ -0,0 +1,136 @@
+package fr.uga.pddl4j.examples.sat;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import fr.uga.pddl4j.examples.sat.HSP;
+import fr.uga.pddl4j.examples.sat.MyPlanner;
+
+
+public class ScriptEvaluation {
+
+    public static void main(String[] args) {
+        
+        /* 
+        File dir = new File(".\\blocks");
+        File[] liste = dir.listFiles(); 
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("blocksTime.txt", true))) {
+            writer.write("probleme;HSPtemps;RWtemps");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("blocksLength.txt", true))) {
+            writer.write("probleme;HSPlength;RWlength");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (File item : liste) {
+            if (!item.getName().toString().equals("domain.pddl")) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("blocksTime.txt", true))) {
+                        writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("blocksLength.txt", true))) {
+                    writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myHSP.main(new String[] { ".\\blocks\\domain.pddl",".\\blocks\\"+item.getName(),"blocksTime.txt","blocksLength.txt" });
+                MyPlanner.main(new String[] { ".\\blocks\\domain.pddl",".\\blocks\\"+item.getName(),"blocksTime.txt","blocksLength.txt" });
+            }
+        }
+        
+        dir = new File(".\\depots");
+        liste = dir.listFiles();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("depotsTime.txt", true))) {
+            writer.write("probleme;HSPtemps;RWtemps");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("depotsLength.txt", true))) {
+            writer.write("probleme;HSPlength;RWlength");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (File item : liste) {
+            if (!item.getName().toString().equals("domain.pddl")) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("depotsTime.txt", true))) {
+                        writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("depotsLength.txt", true))) {
+                    writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myHSP.main(new String[] { ".\\depots\\domain.pddl",".\\depots\\"+item.getName(),"depotsTime.txt","depotsLength.txt" });
+                MyPlanner.main(new String[] { ".\\depots\\domain.pddl",".\\depots\\"+item.getName(),"depotsTime.txt","depotsLength.txt" });
+            }
+        }
+        
+        dir = new File(".\\gripper");
+        liste = dir.listFiles();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("gripperTime.txt", true))) {
+            writer.write("probleme;HSPtemps;RWtemps");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("gripperLength.txt", true))) {
+            writer.write("probleme;HSPlength;RWlength");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (File item : liste) {
+            if (!item.getName().toString().equals("domain.pddl")) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("gripperTime.txt", true))) {
+                        writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("gripperLength.txt", true))) {
+                    writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myHSP.main(new String[] { ".\\gripper\\domain.pddl",".\\gripper\\"+item.getName(),"gripperTime.txt","gripperLength.txt" });
+                MyPlanner.main(new String[] { ".\\gripper\\domain.pddl",".\\gripper\\"+item.getName(),"gripperTime.txt","gripperLength.txt" });
+            }
+        }
+        
+        
+        dir = new File(".\\logistics");
+        liste = dir.listFiles();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("logisticsTime.txt", true))) {
+            writer.write("probleme;HSPtemps;RWtemps");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("logisticsLength.txt", true))) {
+            writer.write("probleme;HSPlength;RWlength");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (File item : liste) {
+            if (!item.getName().toString().equals("domain.pddl")) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("logisticsTime.txt", true))) {
+                        writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter("logisticsLength.txt", true))) {
+                    writer.write("\n"+item.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                myHSP.main(new String[] { ".\\logistics\\domain.pddl",".\\logistics\\"+item.getName(),"logisticsTime.txt","logisticsLength.txt" });
+                MyPlanner.main(new String[] { ".\\logistics\\domain.pddl",".\\logistics\\"+item.getName(),"logisticsTime.txt","logisticsLength.txt" });
+            }
+        }
+        
+        */
+
+    }
+}
